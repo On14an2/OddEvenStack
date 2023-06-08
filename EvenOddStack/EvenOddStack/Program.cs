ed997a6726ce70b4804644ea54dba2015b63bd1e
on14an <112744129+On14an2@users.noreply.github.com>
2023-06-08 12:03:21 +0700

    Add files via upload

---
+++ EvenOddStack/EvenOddStack/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class EvenOddStack
+{
+    public static void Main()
+    {
+        Stack<int> evenStack = new Stack<int>();
+        Stack<int> oddStack = new Stack<int>();
+
+        Console.WriteLine("Введите числа (для завершения введите пустую строку):");
+        string input = null;
+        while (!string.IsNullOrWhiteSpace(input = Console.ReadLine()))
+        {
+            int number = int.Parse(input);
+            if (number % 2 == 0)
+            {
+                evenStack.Push(number);
+            }
+            else
+            {
+                oddStack.Push(number);
+            }
+        }
+
+        Stack<int> sortedStack = new Stack<int>();
+        while (evenStack.Count > 0)
+        {
+            sortedStack.Push(evenStack.Pop());
+        }
+        while (oddStack.Count > 0)
+        {
+            sortedStack.Push(oddStack.Pop());
+        }
+
+        Console.WriteLine("Отсортированные числа:");
+        while (sortedStack.Count > 0)
+        {
+            Console.WriteLine(sortedStack.Pop());
+        }
+    }
+}
